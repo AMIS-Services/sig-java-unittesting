d2368f4d0580d36aecafcdd16f0ad11f18d8aac3
jacco_c <jacco.cijsouw@cissystems.nl>
2021-05-24 12:42:50 +0200

    minor change in import.sql and remove tests

--- NotifierApplication/src/main/resources/import.sql
+++ NotifierApplication/src/main/resources/import.sql
@@ -1,8 +1,10 @@
 insert into subscription (api, subscriber, client_id, session_id, channel_id, subscribed_at, unsubscribed_at, session_removed_at) values ('API01', 'FooChannel##FooSecret', 'Client01', 'FooSession01', 'FooChannel', '2021-05-17 10:15:45.00', null, null);
 
-insert into subscription (api, subscriber, client_id, session_id, channel_id, subscribed_at, unsubscribed_at, session_removed_at) values ('API01', 'BarChannel##BarSecret', 'Client02', 'BarSession01', 'BarChannel02', '2021-05-17 10:16:45.00', '2021-05-17 10:18:45.00', null)
+insert into subscription (api, subscriber, client_id, session_id, channel_id, subscribed_at, unsubscribed_at, session_removed_at) values ('API01', 'FooChannel##FooSecret', 'Client02', 'FooSession02', 'FooChannel', '2021-05-17 10:13:45.00', null, null);
 
-insert into subscription (api, subscriber, client_id, session_id, channel_id, subscribed_at, unsubscribed_at, session_removed_at) values ('API01', 'BazChannel##BazSecret', 'Client03', 'BazSession01', 'BazChannel', '2021-05-17 10:26:45.00', null, null)
+insert into subscription (api, subscriber, client_id, session_id, channel_id, subscribed_at, unsubscribed_at, session_removed_at) values ('API01', 'BarChannel##BarSecret', 'Client03', 'BarSession01', 'BarChannel02', '2021-05-17 10:16:45.00', '2021-05-17 10:18:45.00', null)
+
+insert into subscription (api, subscriber, client_id, session_id, channel_id, subscribed_at, unsubscribed_at, session_removed_at) values ('API01', 'BazChannel##BazSecret', 'Client04', 'BazSession01', 'BazChannel', '2021-05-17 10:26:45.00', null, null)
 
 insert into notification (channel_id, message, created_at ) values ('FooChannel##FooSecret', 'Foo 01', '2021-05-17 10:15:45.00' )
 
